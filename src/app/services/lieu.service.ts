56f0b1a33f6b4971b19241c46a99b478dfcbcb32
Katia ZOUAD <katia.zouad@hotmail.fr>
2025-10-28 11:00:27 +0100

    feat/front-lieux-list:  display list of lieux with API integration and loading/error states

---
+++ src/app/services/lieu.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+export interface Lieu {
+  id: number;
+  nom: string;
+  description: string;
+  adresse: string;
+  dateCreation: string;
+  prixAdulte?: string | null;
+  prixEnfant?: string | null;
+}
+
+// on décrit la forme réelle renvoyée par ton backend
+interface LieuxApiResponse {
+  items: Lieu[];
+  page: number;
+  pageSize: number;
+  total: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LieuService {
+  private apiUrl = 'http://localhost:3000/lieux';
+
+  constructor(private http: HttpClient) {}
+
+  getLieux(): Observable<Lieu[]> {
+    return this.http.get<LieuxApiResponse>(this.apiUrl).pipe(
+      map((response) => response.items)
+    );
+  }
+}
